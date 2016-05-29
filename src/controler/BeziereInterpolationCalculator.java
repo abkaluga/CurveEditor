d847d8db9185e28f3fb5352590944c3f03c26927
Albert <a.b.kaluga@gmail.com>
2016-05-29 08:29:46 +0200

    Beziere interpolation

--- src/controler/BeziereInterpolationCalculator.java
+++ src/controler/BeziereInterpolationCalculator.java
@@ -1,11 +1,16 @@
 package controler;
 
-import model.*;
-import utils.ParametrizationHelper;
+import model.BeziereInterpolated;
+import model.ICurve;
+import model.IPoint;
+import model.Point;
 
+import java.awt.geom.CubicCurve2D;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.lang.Math.sqrt;
 
 /**
  * Created by Albert on 16.05.2016.
@@ -13,8 +18,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class BeziereInterpolationCalculator implements ICalculator {
 
     private static final ICalculator anInstance = new BeziereInterpolationCalculator();
-    private static final BeziereHornerCalculator helper = (BeziereHornerCalculator) BeziereHornerCalculator.getAnInstance();
-    private static final int SEGMENTS_PER_CURVE = 100;
+    private static final double smooth_value = 0.5;
 
     private BeziereInterpolationCalculator() {
 
@@ -27,50 +31,63 @@ public class BeziereInterpolationCalculator implements ICalculator {
     @Override
     public void calculate(ICurve curve, AtomicBoolean dirtyIndicator) {
         if (curve instanceof BeziereInterpolated) {
+            BeziereInterpolated interpolated = (BeziereInterpolated) curve;
+            List<CubicCurve2D> curves = new ArrayList<>();
             List<IPoint> points = curve.getPoints();
-            List<IPoint> newPoints = new ArrayList<>();
+            IPoint p0, p1, p2, p3, ctrl1, ctrl2;
 
-
-            for (int i = 0; i < points.size() - 3; i += 3) {
-                IPoint p0 = points.get(i);
-                IPoint p1 = points.get(i + 1);
-                IPoint p2 = points.get(i + 2);
-                IPoint p3 = points.get(i + 3);
-                for (int j = 0; j <= SEGMENTS_PER_CURVE; j++) {
-                    double t = ParametrizationHelper.getInstance().translate(j, SEGMENTS_PER_CURVE);
-                    newPoints.add(calculateBezierPoint(t, p0, p1, p2, p3));
-                }
-
+            for (int i = 0; i < points.size() - 1; ++i) {
+                p0 = points.get(i - 1 < 0 ? i : i - 1);
+                p1 = points.get(i);
+                p2 = points.get(i + 1);
+                p3 = points.get(i + 2 >= points.size() ? i + 1 : i + 2);
+                ctrl1 = new Point();
+                ctrl2 = new Point();
+                calculateControlPoints(p0, p1, p2, p3, ctrl1, ctrl2);
+                curves.add(new CubicCurve2D.Double(p1.getX(), p1.getY(), ctrl1.getX(), ctrl1.getY(), ctrl2.getX(), ctrl2.getY(), p2.getX(), p2.getY()));
             }
-            Interpolated interpolated = (Interpolated) curve;
-            interpolated.setInterpolatedPoints(newPoints);
+
+            interpolated.setCurves(curves);
             dirtyIndicator.compareAndSet(false, true);
         }
 
 
     }
 
-    public IPoint calculateBezierPoint(double t, IPoint p0, IPoint p1, IPoint p2, IPoint p3) {
-        double u = 1.0 - t;
-        double tt = t * t;
-        double uu = u * u;
-        double uuu = uu * u;
-        double ttt = tt * t;
+    private void calculateControlPoints(IPoint p0, IPoint p1, IPoint p2, IPoint p3, IPoint ctrl1, IPoint ctrl2) {
+        // Assume we need to calculate the control
+        // points between (x1,y1) and (x2,y2).
+        // Then x0,y0 - the previous vertex,
+        //      x3,y3 - the next one.
 
-        double x = uuu * p0.getX(); //first term
-        x += 3 * uu * t * p1.getX(); //second term
-        x += 3 * u * tt * p2.getX(); //third term
-        x += ttt * p3.getX(); //fourth term
-        double y = uuu * p0.getY(); //first term
-        y += 3 * uu * t * p1.getY(); //second term
-        y += 3 * u * tt * p2.getY(); //third term
-        y += ttt * p3.getY(); //fourth term
+        double xc1 = (p0.getX() + p1.getX()) / 2.0;
+        double yc1 = (p0.getY() + p1.getY()) / 2.0;
+        double xc2 = (p1.getX() + p2.getX()) / 2.0;
+        double yc2 = (p1.getY() + p2.getY()) / 2.0;
+        double xc3 = (p2.getX() + p3.getX()) / 2.0;
+        double yc3 = (p2.getY() + p3.getY()) / 2.0;
 
-        IPoint p = new Point();
-        p.setX((int) x);
-        p.setY((int) y);
+        double len1 = sqrt((p1.getX() - p0.getX()) * (p1.getX() - p0.getX()) + (p1.getY() - p0.getY()) * (p1.getY() - p0.getY()));
+        double len2 = sqrt((p2.getX() - p1.getX()) * (p2.getX() - p1.getX()) + (-p1.getY()) * (-p1.getY()));
+        double len3 = sqrt((p3.getX() - p2.getX()) * (p3.getX() - p2.getX()) + (p3.getY() - p3.getY()) * (p3.getY() - p2.getY()));
 
-        return p;
+        double k1 = len1 / (len1 + len2);
+        double k2 = len2 / (len2 + len3);
+
+        double xm1 = xc1 + (xc2 - xc1) * k1;
+        double ym1 = yc1 + (yc2 - yc1) * k1;
+
+        double xm2 = xc2 + (xc3 - xc2) * k2;
+        double ym2 = yc2 + (yc3 - yc2) * k2;
+
+        // Resulting control points. Here smooth_value is mentioned
+        // above coefficient K whose value should be in range [0...1].
+
+        ctrl1.setX((int) (xm1 + (xc2 - xm1) * smooth_value + p1.getX() - xm1));
+        ctrl1.setY((int) (ym1 + (yc2 - ym1) * smooth_value + p1.getY() - ym1));
+
+        ctrl2.setX((int) (xm2 + (xc2 - xm2) * smooth_value + p2.getX() - xm2));
+        ctrl2.setY((int) (ym2 + (yc2 - ym2) * smooth_value + p2.getY() - ym2));
     }
 
 }
--- src/model/BeziereInterpolated.java
+++ src/model/BeziereInterpolated.java
@@ -1,15 +1,30 @@
 package model;
 
+
+import java.awt.*;
+import java.awt.geom.CubicCurve2D;
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by Albert on 16.05.2016.
  */
-public class BeziereInterpolated extends Interpolated {
+public class BeziereInterpolated extends Curve {
     public static final AtomicLong count = new AtomicLong(1);
+    private java.util.List<CubicCurve2D> curves = new ArrayList<>();
+
+    public void setCurves(java.util.List<CubicCurve2D> curves) {
+        this.curves = curves;
+    }
 
     @Override
-    public CurveType getType() {
+    public ICurve.CurveType getType() {
         return ICurve.CurveType.BeziereInterpolated;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        Graphics2D g2D = (Graphics2D) g;
+        curves.forEach(g2D::draw);
     }
 }
